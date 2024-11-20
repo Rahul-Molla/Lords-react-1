f09863c1c0299331788d749462cc55e297038cda
Saniyaj Mallik <saniyajmallik12@gmail.com>
2024-11-20 08:24:40 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+
+  let num = 10
+
+
+  return (
+    // react fragment
+    <>
+      <div>{num + 20}</div>
+      <h1>Welcode to React js</h1>
+
+    </>
+
+  )
+}
+
+export default App
