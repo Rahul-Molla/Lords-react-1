0ac140bbcfb6c831328ceb52fc392036058601db
Saniyaj Mallik <saniyajmallik12@gmail.com>
2024-11-20 20:37:24 +0530

    html structer

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+const Header = () => {
+  return (
+    <div className='header'>
+      Password Saver
+    </div>
+  )
+}
+
+export default Header
