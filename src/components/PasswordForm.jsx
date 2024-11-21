a631a8c7c25a35b42efb9fab1a2c342f992d4146
Saniyaj Mallik <saniyajmallik12@gmail.com>
2024-11-21 20:27:22 +0530

    create function added

--- src/components/PasswordForm.jsx
+++ src/components/PasswordForm.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './PasswordForm.scss'
 
 
-const PasswordForm = () => {
+const PasswordForm = ({onSavePassword}) => {
+
+  const [title, setTitle] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  console.log(title, username, password);
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(title, username, password);
+    
+    onSavePassword({
+      title, username, password
+    })
+
+    setTitle("")
+    setUsername("")
+    setPassword("")
+
+
+  }
+
+
   return (
     <div className='form-div'>
       <h1>Enter details</h1>
       <form action="">
-        <label htmlFor="website">Enter website name</label>
-        <input id='website' type="text" />
+        <label htmlFor="website">Enter a website title</label>
+        <input onChange={(e) => setTitle(e.target.value)} value={title} required id='website' type="text" />
 
         <label htmlFor="title">Enter User name</label>
-        <input id='title' type="text" />
+        <input onChange={(e) => setUsername(e.target.value)} value={username} required id='title' type="text" />
 
         <label htmlFor="password">Enter Password</label>
-        <input id='password' type="text" />
-       
-       <button type='submit'>Save</button>
+        <input onChange={(e) => setPassword(e.target.value)} value={password} required id='password' type="text" />
+
+        <button type='submit' onClick={(e) => handleClick(e)}>Save</button>
       </form>
     </div>
   )
